356c99d58c677727cc30b26d64a61af43342652b
Victor Idam <idamvictorx1@gmail.com>
2025-05-30 14:00:51 +0100

    feat: add button component and utility functions; update styles and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <>
+      <div className="text-red-500">hghghghgfh</div>
+      <Button>Click me</Button>
+    </>
   );
 }
